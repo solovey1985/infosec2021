d58f739a100ad32959ae66a9d7ab281279276ada
Your Name <em@wi.com.ua>
2021-12-13 15:59:31 +0200

    final

---
+++ Task1.1/Task1.1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+//генерація та виведення на екран послідовності псевдовипадкових чисел
+namespace Task1_1{
+class PRNG {
+  static void Main() {
+      Console.WriteLine("The same PRNG\n");
+
+      /*ініціалізуємо змінну класу Random з заданим seed,
+       на основі якого щоразу при запуску програми генерується
+       однакова послідовність рандомних чисел*/
+      Random rnd = new Random(1984);
+      
+      for (int i = 0; i < 13; i++){
+          Console.WriteLine(rnd.Next());
+      }
+      
+      /*в циклі з кожною ітерацією ініціалізується нова змінна класу Random
+      у якої не заданий явний seed, тому при запуску програми послідовність щоразу інша*/
+      Console.WriteLine("\nDifferent PRNG\n");
+      
+      for (int i = 0; i < 13; i++){
+          Random drnd = new Random();
+          Console.WriteLine(drnd.Next());
+      }
+      Console.ReadKey(true);
+  }
+}}
+
---
+++ Task1.2/Task1.2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Task1_2{
+class RNGCSP
+{
+    /*генерація та виведення на екран криптографічно стійкої послідовності випадкових чисел
+     за допомогою класу RNGCryptoServiceProvider
+     (Implements a cryptographic Random Number Generator (RNG) using the implementation
+     provided by the cryptographic service provider (CSP))*/
+    public static void Main()
+    {
+        var rng = new RNGCryptoServiceProvider();
+        var bytes = new byte[8];
+        for(int i = 0; i<13; i++){
+            rng.GetBytes(bytes);
+            Console.WriteLine(BitConverter.ToUInt64(bytes));
+        }
+    Console.ReadKey(true);
+    }
+    }
+    }
